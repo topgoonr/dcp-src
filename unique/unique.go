3b73ca144dcc1aa52c063f84a71e636bd26f9e2b
topgoonr <topgoonr@outlook.com>
2018-09-12 12:02:19 +0530

    Comment correction

--- unique/unique.go
+++ unique/unique.go
@@ -1,7 +1,7 @@
 // Package unique provides the functionality to create unique versions of slices.
 package unique
 
-// Ints returns a unique subset of the int slice provided.
+// Strings returns a unique subset of the string slice provided.
 func Strings(input []string) []string {
 	u := make([]string, 0, len(input))
 	m := make(map[string]bool)
